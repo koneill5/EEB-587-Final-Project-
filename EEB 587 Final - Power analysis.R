ad6de388b9e63ef954dab421a5bb2882ae5e1f27
koneill5 <78922229+koneill5@users.noreply.github.com>
2022-05-05 13:31:57 -0400

    Add files via upload

---
+++ EEB 587 Final - Power analysis.R
@@ -0,0 +1,21 @@
+# EEB 587 Final Project - due the last day of finals 
+
+
+#Power analysis to decide the appropriate number/range of taxa needed for dissertation work 
+
+# Question 1 - 
+
+# Question 2 - 
+
+#Methods/steps:
+# Test the Boettiget, Coop, and Ralph, 2012 (Data from: Is your phylogeny informative? Measuring the power of comparative methods) package 
+# 
+
+# 
+
+library(ape)
+library(geiger)
+primate.tree.test <- "TreeBlock_10kTrees_Primates_Version3.nex"
+ape::read.nexus(primate.tree.test)
+
+
